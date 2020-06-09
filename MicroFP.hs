2c1e9396498659981261fb6b57b723b3f2cfe192
Ruilin Yang <r.yang-1@student.utwente.nl>
2020-06-09 16:31:50 +0200

    3.3 3.4 formatted; tests of intermediate result of 3.4 removed (I assume they only cares about the top-level

--- MicroFP.hs
+++ MicroFP.hs
@@ -77,39 +77,38 @@ eleven = Assign "eleven" [] (FunCall "inc" [(Constant 10)])
 
 -- ---------- FP3.3 ----------
 
+-- ----- tier 0: the top-most level -----
+
 {-
-`pretty` - prints input of `Func` data type in a readable way
+    `pretty` takes care of things of `Func` data type
+    `oneExpr` and `manyExpr` deals with things of `Expr` type
 -}
 pretty :: Func -> String 
 pretty (Assign funcname args funcbody) = funcname ++ " " ++ argString ++ " := "++ funcbodyString ++ ";"
     where argString      = manyExpr args " "
           funcbodyString = oneExpr funcbody
 
-{-
-`oneExpr` and `manyExpr` - take care of input of `Expr` data type in order to pass it to pretty
 
-manyExpr - transforms a list of Expr into strings separated by whitespace
-Params:
-1 [Expr]: the list of expressions to be stringified
-2 String: the separator: in case of function definition it should be " ";
-                         in case of function call it should be ", ".
-3 String: the final result
+-- ----- tier 1: helpers of `pretty`: `manyExpr`, `oneExpr`, `manyExprHelper`, `condString` -----
+{-
+    transform a list of Expr into strings separated by whitespace
+    Params: 
+    1 [Expr]: the list of expression to be stringified
+    2 String: the separator, in case of function definition it should be " "; 
+                           in case of function call it should be ", ".
+    3 String: the final result
 -}
 manyExpr :: [Expr] -> String -> String 
 manyExpr expr sep = trim toTrim
     where toTrim = manyExprHelper expr sep
           trim   = (\input -> let len = (length input) - (length sep) in   -- trim the trailing separator
                               take len input)
-{-
-manyExprHelper - helper of `manyExpr` resulting in expressions joined by separators
--}
+
 manyExprHelper :: [Expr] -> String -> String 
 manyExprHelper [] sep     = []
 manyExprHelper (x:xs) sep = (oneExpr x) ++ sep ++ (manyExprHelper xs sep) 
 
-{-
-oneExpr - transforms expressions into readable text using pattern matching
--}
+
 oneExpr :: Expr -> String
 oneExpr (Constant c) = show c 
 oneExpr (Var v)      = v
@@ -118,7 +117,7 @@ oneExpr (Sub e1 e2)  = (oneExpr e1) ++ " - " ++ (oneExpr e2)
 oneExpr (Mul e1 e2)  = (oneExpr e1) ++ " * " ++ (oneExpr e2) 
 oneExpr (FunCall fname args)  = fname ++ 
                                 " (" ++ 
-                                (manyExpr args ", ") ++ -- args here should be separated by commas
+                                (manyExpr args ", ") ++ -- args in FunCall should be separated by comma 
                                 ")"  
 oneExpr (If cond eThen eElse) =  "if " ++ 
                                  (condString cond) ++ 
@@ -127,9 +126,9 @@ oneExpr (If cond eThen eElse) =  "if " ++
                                  "\n} else {\n\t" ++ 
                                  (oneExpr eElse) ++
                                  "\n}"
-
 {-
-condString - helper of `oneExpr` resulting in a string of conditions surrounded by parentheses
+    helper of `oneExpr`
+    resulting in a string of condition surrounded by ()
 -}
 condString :: Cond -> String 
 condString (Cond e1 ord e2) = "(" ++ (oneExpr e1) ++ " " ++ (show ord) ++ " " ++ (oneExpr e2) ++ ")"
@@ -137,22 +136,28 @@ condString (Cond e1 ord e2) = "(" ++ (oneExpr e1) ++ " " ++ (show ord) ++ " " ++
 
 -- ---------- FP3.4 ----------
 
+
 {-
-eval - evaluates the functions represented with our data types
+    about functions: 
+        basic evaluation:         `fib` `div` `add`    can evaluate
+        need pattern matching:    `sum` `fibonacci`    doesn't support 
+        need partial application: `inc`                doesn't support
+        need higher order func:   `twice`              doesn't support
 -}
+
 -- ----- tier 0: the top-most level -----
-eval :: Func -> [Expr] -> [Expr] -> Expr  -- final end result should be Integer
-eval (Assign fname argsF fbody) args vals = reduceOneExpr (bindOneExpr fbody args vals) func
+eval :: Func -> [Expr] -> [Expr] -> Expr 
+eval (Assign fname argsF fbody) args vals = reduceOneExpr (bindOneExpr fbody args vals) func 
     where func = Assign fname argsF fbody
 
 
 -- ----- tier 1: helpers of tier 0 functions -----
 {-
-Params:
-1 Expr:   the expression containing variables
-2 [Expr]: the variables of concern
-3 [Expr]: the values to be bound to the variables
-4 Expr:   the resulting expression
+    Params: 
+    1 Expr:   the expression containing variables
+    2 [Expr]: the variables of concern
+    3 [Expr]: the values to be bind to the variables
+    4 Expr:   resulting expression
 -}
 bindOneExpr :: Expr -> [Expr] -> [Expr] -> Expr
 bindOneExpr (Constant i)  _ _       = Constant i
@@ -170,11 +175,11 @@ bindOneExpr (If (Cond e1 order e2) eThen eElse) args vals = (If (Cond e1' order 
 
 
 {-
-Params:
-1 [Expr]: the list of expressions that contain variables
-2 [Expr]: the variables of concern
-3 [Expr]: the values to be bound to the variables
-4 [Expr]: the resulting list of Expr after binding
+    Params: 
+    1 [Expr]: the list of expressions that contain variables 
+    2 [Expr]: the variables of concern
+    3 [Expr]: the values to be bind to the variables 
+    4 [Expr]: the resulting list of Expr after binding
 -}
 bindListExpr :: [Expr] -> [Expr] -> [Expr] -> [Expr]
 bindListExpr [] _ _           = []
@@ -190,12 +195,12 @@ reduceOneExpr (Sub (Constant c1) (Constant c2))  func  = Constant (c1-c2)
 reduceOneExpr (Sub e1 e2)                        func  = Sub (reduceOneExpr e1 func) (reduceOneExpr e2 func)
 reduceOneExpr (Mul (Constant c1) (Constant c2))  func  = Constant (c1*c2)
 reduceOneExpr (Mul e1 e2)                        func  = Mul (reduceOneExpr e1 func) (reduceOneExpr e2 func)
-reduceOneExpr (FunCall s args) (Assign fname argsF fbody)  | funNoVar   =  reduceOneExpr (bindOneExpr fbody argsF reducedArgs) func
+reduceOneExpr (FunCall s args) (Assign fname argsF fbody)  | funNoVar   =  reduceOneExpr (bindOneExpr fbody argsF reducedArgs) func 
                                                            | otherwise  =  reduced
                                                            where func        = Assign fname argsF fbody
                                                                  reducedArgs = reduceListExpr args func
                                                                  reduced     = FunCall s reducedArgs
-                                                                 funNoVar    = hasNoVarExpr reduced
+                                                                 funNoVar    = hasNoVarExpr reduced 
 reduceOneExpr (If (Cond e1 order e2) eThen eElse) func | condNoVar && condTrue      = reduceOneExpr eThen' func
                                                        | condNoVar && not(condTrue) = reduceOneExpr eElse' func
                                                        | otherwise                  = reduced
@@ -210,6 +215,7 @@ reduceListExpr (x:xs) func = reduceOneExpr x func : reduceListExpr xs func
 
 
 -- ----- tier 2: helpers of tier 1 functions -----
+
 hasNoVarExpr :: Expr -> Bool
 hasNoVarExpr (Constant i)  = True
 hasNoVarExpr (Var v)       = False
@@ -220,16 +226,16 @@ hasNoVarExpr (FunCall s args) = all hasNoVarExpr args
 hasNoVarExpr (If (Cond e1 od e2) eThen eElse) = all hasNoVarExpr [e1, e2, eThen, eElse]
 
 
-hasNoVarCond :: Cond -> Bool
+hasNoVarCond :: Cond -> Bool 
 hasNoVarCond (Cond e1 od e2) = all hasNoVarExpr [e1, e2]
 
 
-evalCond :: Cond -> Bool
+evalCond :: Cond -> Bool 
 evalCond (Cond e1 od e2) | od == Gt   = (evalExpr e1) >  (evalExpr e2)
-                         | od == Eq   = (evalExpr e1) == (evalExpr e2)
+                         | od == Eq   = (evalExpr e1) == (evalExpr e2) 
                          | otherwise  = (evalExpr e1) <  (evalExpr e2)
 
-evalExpr :: Expr -> Integer
+evalExpr :: Expr -> Integer 
 evalExpr (Constant i) = i
 
 
@@ -310,76 +316,32 @@ pEleven = putStrLn $ pretty eleven
 
 
 -- ----- test FP3.4 -----
-{-
-the following show how binding works on some examples
--}
-bindXYZ = [(Var "x"), (Var "y"), (Var "z")]
-bindXY  = [(Var "x"), (Var "y")]
-bindXZ  = [(Var "x"), (Var "z")]
-bindX   = [(Var "x")]
-val3    = [(Constant 10), (Constant 20), (Constant 30)]
-val2    = [(Constant 105), (Constant 10)]
-val1    = [(Constant 8)]
-
-{-
-the following show more complex binding
--}
-testBindConst = putStrLn $ oneExpr $ bindOneExpr (Constant 99) bindX val3
-testBindVar   = putStrLn $ oneExpr $ bindOneExpr (Var "x") bindXY val2
-testBindVar'  = putStrLn $ oneExpr $ bindOneExpr (Var "z") bindXY val2
-testBindAdd   = putStrLn $ oneExpr $ bindOneExpr (Add (Var "x") (Var "y")) bindXY val3
-testBindSub   = putStrLn $ oneExpr $ bindOneExpr (Sub (Var "x") (Var "z")) bindXY val2
-testBindMul   = putStrLn $ oneExpr $ bindOneExpr (Mul (Constant 99) (Var "y")) bindXY val2
-
-
--- bind and evaluate `fib` function
-valFib = [Constant 5]
-testBindFibRaw :: Expr
-testBindFibRaw = bindOneExpr fbody bindXY valFib
-    where fbody = (If (Cond (Var "x") Lt (Constant 3)) (Constant 1) (Add (FunCall "fib" [(Sub (Var "x") (Constant 1))]) (FunCall "fib" [(Sub (Var "x") (Constant 2))])))
-testBindFib :: IO ()
-testBindFib = putStrLn $ oneExpr $ testBindFibRaw
-
-
-testReduceFibRaw :: Expr
-testReduceFibRaw = reduceOneExpr testBindFibRaw func
-    where func = Assign "fib" [(Var "x")] (If (Cond (Var "x") Lt (Constant 3)) (Constant 1) (Add (FunCall "fib" [(Sub (Var "x") (Constant 1))]) (FunCall "fib" [(Sub (Var "x") (Constant 2))])))
-testReduceFib :: IO ()
-testReduceFib = putStrLn $ oneExpr $ testReduceFibRaw
-
-
-
--- bind and evaluate `div` function
-valDiv = [Constant 105, Constant 10]
-testBindDivRaw :: Expr
-testBindDivRaw = bindOneExpr divbody bindXY valDiv
-    where divbody = (If (Cond (Var "x") Lt (Var "y")) (Constant 0) (Add (Constant 1) (FunCall "div" [(Sub (Var "x") (Var "y")), (Var "y")])))
-testBindDiv :: IO ()
-testBindDiv = putStrLn $ oneExpr testBindDivRaw
-
-
-testReduceDivRaw :: Expr
-testReduceDivRaw = reduceOneExpr testBindDivRaw func
-    where func = Assign "div" [(Var "x"), (Var "y")] (If (Cond (Var "x") Lt (Var "y")) (Constant 0) (Add (Constant 1) (FunCall "div" [(Sub (Var "x") (Var "y")), (Var "y")])))
-testReduceDiv :: IO ()
-testReduceDiv = putStrLn $ oneExpr $ testReduceDivRaw
-
 
 -- evaluation of `fib` `div` and `add`, when all arguments are supplied with value
-testEvalFib :: Expr
+
+-- eval `fib
+argFib = [Var "n"]
 valEvalFib = [Constant 10]
-testEvalFib = eval func [Var "n"] valEvalFib
-    where func = (Assign "fib" [(Var "n")] (If (Cond (Var "n") Lt (Constant 3)) (Constant 1) (Add (FunCall "fib" [(Sub (Var "n") (Constant 1))]) (FunCall "fib" [(Sub (Var "n") (Constant 2))]))))
+
+testEvalFib :: Expr
+testEvalFib = eval func argFib valEvalFib
+    where func = fib
+
+-- eval `div`
+argDiv = [(Var "x"), (Var "y")]
+valEvalDiv = [Constant 66, Constant 15]
 
 testEvalDiv :: Expr
-valEvalDiv = [Constant 50, Constant 15]
-testEvalDiv = eval func [(Var "x"), (Var "y")] valEvalDiv
-    where func = Assign "div" [(Var "x"), (Var "y")] (If (Cond (Var "x") Lt (Var "y")) (Constant 0) (Add (Constant 1) (FunCall "div" [(Sub (Var "x") (Var "y")), (Var "y")])))
+testEvalDiv = eval func argDiv valEvalDiv
+    where func = div'
+
+-- eval `add`
+argAdd      = [(Var "x"), (Var "y")]
+valEvalAdd  = [Constant 99, Constant 10]
 
 testEvalAdd :: Expr
-valEvalAdd = [Constant 34, Constant 12]
-testEvalAdd = eval func [(Var "x"), (Var "y")] valEvalAdd
-    where func = Assign "add" [(Var "x"), (Var "y")] (Add (Var "x") (Var "y"))
+testEvalAdd = eval func argAdd valEvalAdd
+    where func = add
 
 -- ----- Test FP4.1 ------
 
@@ -387,17 +349,17 @@ testEvalAdd = eval func [(Var "x"), (Var "y")] valEvalAdd
 running tests: testSomeFunctionName (Stream "input to parse")
 -}
 
-testFactor = runParser $ factor -- will parse integers, 'if'-statements, function calls, identifiers or expressions in parentheses
+testFactor = runParser $ factor    -- will parse integers, 'if'-statements, function calls, identifiers or expressions in parentheses
 
-testExpr = runParser $ expr -- will parse addition, substraction or terms
+testExpr = runParser $ expr        -- will parse addition, substraction or terms
 
-testTerm = runParser $ term -- will parse multiplication or factors
+testTerm = runParser $ term        -- will parse multiplication or factors
 
-testCond = runParser $ cond -- will parse conditions, e.g. "a>b"
+testCond = runParser $ cond        -- will parse conditions, e.g. "a>b"
 
-testFunc = runParser $ func -- will parse function definitions
+testFunc = runParser $ func        -- will parse function definitions
 
-testOrder = runParser $ order -- will parse order, e.g. "=="
+testOrder = runParser $ order      -- will parse order, e.g. "=="
 
 {-
 the following tests can be combined with their counterparts in the block
@@ -411,7 +373,7 @@ of a function. Example:
     "fibonacci n := fibonacci (n - 1) + fibonacci (n - 2);"
 -}
 {-
-the parsed functions:
+from String to parsing result 
 -}
 testFibo    = testFunc $ Stream $ pretty fibonacci
 testFib     = testFunc $ Stream $ pretty fib
@@ -424,7 +386,7 @@ testEleven  = testFunc $ Stream $ pretty eleven
 
 
 {-
-the readable counterparts:
+from parsed result to String
 -}
 testFibo'  = pretty $ fst $ head $ testFibo
 testFib'   = pretty $ fst $ head $ testFib
